525bcb5163fd0bccba9437386a624bffefe2847b
Cameron McEwan <cameron@solomonpierce.com>
2015-11-13 01:49:27 +0900

    Got it working, starting to cleanup

---
+++ Mp4HeaderFix/FoundBytes.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mp4HeaderFix
+{
+  class FoundBytes
+  {
+    private byte[] patternToFind;
+    private byte[] patternToSearch;
+    private int firstPatternIndex;
+
+    public bool IsMatch
+    {
+      get
+      {
+        IndexOfSequence();
+
+        if (this.firstPatternIndex > 0)
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
+    }
+
+    public int PatternIndex
+    {
+      get
+      {
+        IndexOfSequence();
+        return this.firstPatternIndex;
+      }
+    }
+
+    public FoundBytes(byte[] patternToFind, byte[] patternToSearch)
+    {
+      this.patternToFind = patternToFind;
+      this.patternToSearch = patternToSearch;
+      this.firstPatternIndex = 0;
+    }
+
+    private void IndexOfSequence()
+    {
+      // We only need to run this once per file
+      if (this.firstPatternIndex != 0)
+      {
+        return;
+      }
+
+      // Find the first occurance of the first element of the pattern we're searching for
+      int i = IndexOfByte(this.patternToSearch, this.patternToFind[0]);
+
+      while (i >= 0 && i <= this.patternToSearch.Length - this.patternToFind.Length)
+      {
+        // Copy the potential match into a new byte array
+        byte[] segment = new byte[this.patternToFind.Length];
+        Buffer.BlockCopy(this.patternToSearch, i, segment, 0, this.patternToFind.Length);
+
+        // See if this new array matches our pattern
+        if (segment.SequenceEqual<byte>(this.patternToFind))
+        {
+          this.firstPatternIndex = i;
+          break;
+        }
+
+        // Move onto the next occurance of the first element of the pattern we're searching for
+        i = Array.IndexOf<byte>(this.patternToSearch, this.patternToFind[0], i + this.patternToFind.Length);
+      }
+    }
+
+    private int IndexOfByte(byte[] arrayToSearch, byte value)
+    {
+      for (int i = 0; i < arrayToSearch.Length; i++)
+      {
+        if (arrayToSearch[i] == value)
+        {
+          return i;
+        }
+      }
+      return -1;
+    }
+
+  }
+}
--- Mp4HeaderFix/Program.cs
+++ Mp4HeaderFix/Program.cs
@@ -1,64 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace Mp4HeaderFix
 {
-  class Program
+  static class Program
   {
     static void Main(string[] args)
     {
-      byte[] oldDimensions = new byte[] { 0x01, 0x40, 0x00, 0xF0 };
-      byte[] newData = new byte[] { 0x02, 0x80, 0x01, 0xE0 };
-      string filename = @"c:\temp.mp4";
-
-      BinaryFile originalFile = new BinaryFile(filename);
-
       Console.WriteLine("Starting to read binary file...");
 
+      byte[] oldDimensions = new byte[] { 0x01, 0x40, 0x00, 0xF0 };
+      byte[] newDimensions = new byte[] { 0x02, 0x80, 0x01, 0xE0 };
+      string filename = @"c:\temp.mp4";
+      string newFilename = @"c:\tempFIX.mp4";
+
+
+      byte[] originalFile = File.ReadAllBytes(filename);
+      byte[] newFile = originalFile.ReplaceBytes(oldDimensions, newDimensions);
+
+      WriteFile(newFilename, newFile);
+
+      EndProgram();
+    }
+
+    private static void WriteFile(string newFilename, byte[] newFile)
+    {
+      FileStream fileStream = new FileStream(newFilename, FileMode.Create, FileAccess.Write);
+      fileStream.Write(newFile, 0, newFile.Length);
+      fileStream.Close();
+    }
+
+    private static byte[] ReplaceBytes(this byte[] originalFile, byte[] oldDimensions, byte[] newData)
+    {
+      byte[] result = originalFile;
+
+      FoundBytes foundBytes = new FoundBytes(oldDimensions, originalFile);
+
+      if (foundBytes.IsMatch)
+      {
+        Console.WriteLine("Found at index: " + foundBytes.PatternIndex);
+        result[foundBytes.PatternIndex] = newData[0];
+        result[foundBytes.PatternIndex + 1] = newData[1];
+        result[foundBytes.PatternIndex + 2] = newData[2];
+        result[foundBytes.PatternIndex + 3] = newData[3];
+      }
+      else
+      {
+        Console.WriteLine("Could not find the specified directions in the source file.");
+      }
+
+      return result;
+    }
+
+    private static void EndProgram()
+    {
       Console.WriteLine("Press enter to quit.");
       Console.ReadLine();
     }
 
-    static int findResolution(byte[] file, byte[] dimensions)
-    {
-      int result = 0;
-
-      bool foundSequence = false;
-      int sequenceindexStart = 0;
-
-      for (int i = 0; i < file.Length; i++)
-      {
-        if (file[i] == dimensions[0])
-        {
-          Console.WriteLine("Found first byte.");
-          sequenceindexStart = i;
-          if (file[i + 1] == dimensions[1])
-          {
-            Console.WriteLine("Found second byte.");
-            if (file[i + 2] == dimensions[2])
-            {
-              Console.WriteLine("Found third byte.");
-              if (file[i + 3] == dimensions[3])
-              {
-                foundSequence = true;
-                Console.WriteLine("Found fourth byte.");
-                Console.WriteLine("Complete hex sequence found at index " + sequenceindexStart.ToString());
-              }
-            }
-          }
-
-          if (foundSequence)
-          {
-            break;
-          }
-        }
-
-        return result;
-      }
-    }
   }
 }
