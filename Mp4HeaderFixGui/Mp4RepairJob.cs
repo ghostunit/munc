2b111f19e3df046f678bc691d4d455abc88608db
Cameron McEwan <cameron@solomonpierce.com>
2015-11-15 17:43:42 +0900

    Working on the GUI a bit more

---
+++ Mp4HeaderFixGui/Mp4RepairJob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mp4HeaderFixGui
+{
+  class Mp4RepairJob
+  {
+    private string pathToOriginalFiles;
+    private UInt16 originalDimensionsWidth;
+    private UInt16 originalDimensionsHeight;
+    private UInt16 actualDimensionsWidth;
+    private UInt16 actualDimensionsHeight;
+    private string filenamePrefix;
+    private string filenameSuffix;
+    private string savePath;
+  }
+}
