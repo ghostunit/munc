1ddb0c81e9ac83597b432f3e8529908564632101
Cameron McEwan <cameron@solomonpierce.com>
2015-11-17 16:19:16 +0900

    Added some validation methods

--- Mp4HeaderFixGui/MainWindow.xaml.cs
+++ Mp4HeaderFixGui/MainWindow.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Input;
 using Mp4HeaderFix;
 using System.Windows.Controls;
+using Xceed.Wpf.Toolkit;
+using System.Windows.Media;
 
 namespace Mp4HeaderFixGui
 {
@@ -15,9 +16,28 @@ namespace Mp4HeaderFixGui
     {
       InitializeComponent();
 
-      this.btnSelectSource.MouseUp += btnSelectSource_MouseUp;
+      #region Event Subscriptions
+
+      // Validate paths
+      this.txtSourcePath.TextChanged += txtSourcePath_ValidateSourcePath;
+      this.txtDestinationPath.TextChanged += txtDestinationPath_ValidateDestinationPath;
+
+      // Validate dimensions
+      this.txtOriginalDimensionsWidth.TextChanged += txtDimensions_ValidateDimension;
+      this.txtOriginalDimensionsHeight.TextChanged += txtDimensions_ValidateDimension;
+      this.txtActualDimensionsWidth.TextChanged += txtDimensions_ValidateDimension;
+      this.txtActualDimensionsHeight.TextChanged += txtDimensions_ValidateDimension;
+
+      // Validate filename modifiers
+      this.txtFilenamePrefix.TextChanged += txtFilenamePrefix_ValidatePrefix;
+      this.txtFilenameSuffix.TextChanged += txtFilenameSuffix_ValidateSuffix;
+
+      // Display modified filename
       this.txtFilenamePrefix.TextChanged += txtFilenamePrefixOrSuffix_TextChanged;
       this.txtFilenameSuffix.TextChanged += txtFilenamePrefixOrSuffix_TextChanged;
+
+      #endregion
+
     }
 
     private Mp4RepairJob LaunchRepairJob()
@@ -28,15 +48,88 @@ namespace Mp4HeaderFixGui
       return new Mp4RepairJob(this.txtSourcePath.Text, orginalDimensions, actualDimensions, fileSaveRule);
     }
 
-    private void btnSelectSource_MouseUp(object sender, MouseButtonEventArgs e)
+    #region Event Methods
+
+    #region Validation Event Methods
+
+    private void txtFilenamePrefix_ValidatePrefix(object sender, EventArgs e)
     {
-      this.txtSourcePath.Text = "Hello, world";
+      WatermarkTextBox textBox = sender as WatermarkTextBox;
+
+      if (textBox.Text.IsValidFilenamePrefix())
+      {
+        textBox.ClearValue(Border.BorderBrushProperty);
+      }
+      else
+      {
+        textBox.BorderBrush = Brushes.Red;
+      }
     }
+
+    private void txtFilenameSuffix_ValidateSuffix(object sender, EventArgs e)
+    {
+      WatermarkTextBox textBox = sender as WatermarkTextBox;
+
+      if (textBox.Text.IsValidFilenameSuffix())
+      {
+        textBox.ClearValue(Border.BorderBrushProperty);
+      }
+      else
+      {
+        textBox.BorderBrush = Brushes.Red;
+      }
+    }
+
+    private void txtDimensions_ValidateDimension(object sender, EventArgs e)
+    {
+      WatermarkTextBox textBox = sender as WatermarkTextBox;
+
+      if (textBox.Text.IsValidDimension())
+      {
+        textBox.ClearValue(Border.BorderBrushProperty);
+      }
+      else
+      {
+        textBox.BorderBrush = Brushes.Red;
+      }
+    }
+
+    private void txtSourcePath_ValidateSourcePath(object sender, EventArgs e)
+    {
+      WatermarkTextBox textBox = sender as WatermarkTextBox;
+
+      if (textBox.Text.IsValidSourcePath())
+      {
+        textBox.ClearValue(Border.BorderBrushProperty);
+      }
+      else
+      {
+        textBox.BorderBrush = Brushes.Red;
+      }
+    }
+
+    private void txtDestinationPath_ValidateDestinationPath(object sender, EventArgs e)
+    {
+      WatermarkTextBox textBox = sender as WatermarkTextBox;
+
+      if (textBox.Text.IsValidDestinationPath())
+      {
+        textBox.ClearValue(Border.BorderBrushProperty);
+      }
+      else
+      {
+        textBox.BorderBrush = Brushes.Red;
+      }
+    }
+
+    #endregion
 
     private void txtFilenamePrefixOrSuffix_TextChanged(object sender, EventArgs e)
     {
       this.lblExampleFilename.Text = this.txtFilenamePrefix.Text + "ExampleFilename" + this.txtFilenameSuffix.Text;
     }
+
+    #endregion
 
   }
 }
---
+++ Mp4HeaderFixGui/Validation.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Mp4HeaderFixGui
+{
+  internal static class Validation
+  {
+    internal static bool IsValidDimension(this string stringToTest)
+    {
+      bool result = false;
+      UInt16 temp;
+
+      if (UInt16.TryParse(stringToTest, out temp))
+      {
+        result = true;
+      }
+
+      return result;
+    }
+
+    internal static bool IsValidFilenamePrefix(this string stringToTest)
+    {
+      bool result = true;
+
+      if (String.IsNullOrEmpty(stringToTest))
+      {
+        return true;
+      }
+
+      if (!Char.IsLetter(stringToTest[0]))
+      {
+        result = false;
+      }
+      
+      if (stringToTest.Any(Path.GetInvalidFileNameChars().Contains))
+      {
+        result = false;
+      }
+
+      return result;
+    }
+
+    internal static bool IsValidFilenameSuffix(this string stringToTest)
+    {
+      bool result = true;
+
+      if (String.IsNullOrEmpty(stringToTest))
+      {
+        return true;
+      }
+
+      if (stringToTest.Any(Path.GetInvalidFileNameChars().Contains))
+      {
+        result = false;
+      }
+
+      return result;
+    }
+
+    internal static bool IsValidSourcePath(this string stringToTest)
+    {
+      bool result = true;
+
+      if (String.IsNullOrEmpty(stringToTest))
+      {
+        result = false;
+      }
+
+      try
+      {
+        Path.GetFullPath(stringToTest);
+      }
+      catch (Exception ex)
+      {
+        result = false;
+      }
+
+      return result;
+    }
+
+    internal static bool IsValidDestinationPath(this string stringToTest)
+    {
+      bool result = false;
+
+      if (String.IsNullOrEmpty(stringToTest))
+      {
+        result = false;
+      }
+
+      if (stringToTest.Any(Path.GetInvalidFileNameChars().Contains))
+      {
+        result = false;
+      }
+
+      return result;
+    }
+
+  }
+}
